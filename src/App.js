838d632b49c6105aadd10e6c96cf453d7412139a
SmartCoding <ferreiratorres.alejandra@gmail.com>
2019-04-28 22:10:58 +0200

    Component fetch current weather from openweatherapi.com

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header.js';
-import WheatherDisplay from './components/WeatherDisplay.js';
+import WeatherDisplay from './components/WeatherDisplay.js';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <WheatherDisplay />
+      <WeatherDisplay />
     </div>
   );
 }
--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,45 +1,61 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 
-const url = 'api.openweathermap.org/data/2.5/weather?q={'city'}, 
-const APP_APIkey = '828cab073a2681f16e65a7058f2f8b1e'
-const getJson = function(response) {
-    return response.json();
-}
 
-class WeatherDisplay extends Component {
-    constructor (props) {
-        super(props)
-        this.state = {
-            wheather: {}
-        }
-    }
-    componentDidMount = () => {
-        //const promesaDelApi = await fetch()
-        //const respuestaDelApi = await promesaDelApi.json()
-        fetch(url)
-            .then(getJson)
-            //.then(getResponse)
-            .then( (wheatherResponse) => {
-                this.setState({wheather: wheatherResponse})
-            })
-    }
-    changeCity (event) {
+const WeatherDisplay = () => {
+    const url = 'http://api.openweathermap.org/data/2.5/weather?';
+    const APP_IDKEY = '828cab073a2681f16e65a7058f2f8b1e';
 
+    const [weather, setWeather] = useState({});
+    const [cityName, setCityName] = useState('London')
+    const [countryCode, setCountryCode] = useState('uk')
+
+    useEffect(() => {
+        getCurrentWeather(cityName, countryCode);
+    }, []);
+
+    const getCurrentWeather = async (cityName, countryCode) => {
+        const response = await fetch(url + 'q=' + cityName + ',' + countryCode + '&APPID=' + APP_IDKEY);
+        const data = await response.json();
+        setWeather(data)
+    };
+
+    const changeCity = (eventCity) => {
+        const newCity = eventCity.target.value
+        setCityName(newCity)
+        getCurrentWeather(newCity, countryCode)
+        console.log(eventCity, eventCity.target.value)
     }
-    render () {
-        return (
+
+    const changeCountry = (eventCountry) => {
+        const newCountry = eventCountry.target.value
+        setCountryCode(newCountry)
+        getCurrentWeather(cityName, newCountry)
+    }
+
+    return (
+        <div className="App">
+            <form className="search-form">
+                <div>
+                    <label htmlFor="city-name">City</label>
+                    <input id='city-name' className="search-bar" type="text" onChange={changeCity}/>
+                </div>
+                <div>
+                    <label htmlFor="country-code">Country Code</label>
+                    <input id='country-code' className="search-bar" type="text" onChange={changeCountry}/>
+                </div>
+                
+                <button className="search-button" type="submit">
+                    submit
+                </button>
+            </form>
             <div>
-                <label htmlFor="city-name">City: </label>
-                <input id='city-name' type="text" onChange={this.changeCity}/>
-                <Display city={this.state.wheather.name}></Display>
+                {JSON.stringify(weather)}
             </div>
-        )
-    }
-}
-
-const Display = props => {
-    const city = props.city
-    return <div>Sunny {city}</div>
+            <div>
+                {cityName} - {countryCode}
+            </div>
+        </div>
+    );
 }
 
 export default WeatherDisplay
